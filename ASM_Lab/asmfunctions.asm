0336fc5ea39885be1c8e9532a58138f70db1756a
C-Timm <christopher.timm@student.dwc.edu>
2016-10-16 15:42:30 -0400

    Create asmfunctions.asm

---
+++ ASM_Lab/asmfunctions.asm
@@ -0,0 +1,28 @@
+; asmfunctions.asm
+.xmm
+.model flat, c
+.data
+
+.code
+
+
+FindSmallest proc export
+	mov edx, dword ptr [esp+4] ; edx = *int
+	mov ecx, dword ptr [esp+8] ; ecx = Count
+	mov eax, 7fffffffh ; eax will be our answer
+	cmp ecx, 0 ; Are there 0 items?
+	jle Finished ; If so we're done
+
+MainLoop:
+	
+	cmp dword ptr [edx], eax ; Is *edx < eax?
+	cmovl eax, dword ptr [edx]; If so, eax = edx
+	add edx, 4 ; Move *edx to next int
+
+	dec ecx ; Decrement counter
+	jnz MainLoop ; Loop if there's more
+	Finished:
+	ret ; Return with lowest in eax
+
+FindSmallest endp
+end
